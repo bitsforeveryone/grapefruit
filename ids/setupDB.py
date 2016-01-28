5468d5297287a163517994c43fe80d7bb5d732fd
edwood777 <edwood777@gmail.com>
2016-01-28 17:48:56 -0500

    database stuff

---
+++ ids/setupDB.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+def setupDB():
+	with app.app_context():
+		print "Setting up DB..."
+		db = sqlite3.connect('db.db')
+		db.cursor().execute("""CREATE TABLE conversations ( 
+							filename text primary key,
+			                time timestamp,
+			                size integer,
+			                s_port integer,
+			                d_port integer,
+			                s_ip text,
+			                d_ip text,
+			                comment text,
+			                flags integer,
+			                service text, 
+			                round integer, 
+			                length integer,
+			                foreign key(round) references rounds(num), 
+			                foreign key(service) references service(name))
+							""")
+		db.cursor().execute("""CREATE TABLE rounds (
+							num integer primary key unique, 
+							s_time timestamp unique)
+							""")
+		db.cursor().execute("""CREATE TABLE services (
+							id integer auto increment unique,
+							name text, 
+							port integer)
+							""")
+		db.commit()
+
+setupDB()
