6f9830d08e7ff6ace02820cd4af2d11c4221e74d
lassimus <jessie@lassimus.com>
2016-02-05 15:29:47 -0500

    cronme

--- ids/run.py
+++ ids/run.py
@@ -187,4 +187,4 @@ def index():
 	return render_template("pages/index.html", roundNum=getNumRounds(), serviceNum=getNumServices(), alertNum=getNumAlerts())
 	
 if __name__ == '__main__':
-    app.run(debug=DEBUG,host="0.0.0.0",port=80)
+    app.run(debug=DEBUG,host="0.0.0.0",port=5000)
