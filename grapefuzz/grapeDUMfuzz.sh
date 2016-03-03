0e821bdd88d916c1821fe90daf6c191855335671
I-am-Batman <clmjr78@gmail.com>
2016-03-02 19:32:30 -0500

    add note to grapeDUMfuzz.sh

--- grapefuzz/grapeDUMfuzz.sh
+++ grapefuzz/grapeDUMfuzz.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+#This is a simple example of how to harness a fuzzer to fuzz a large number of binaries. 
+
 if [ $# -eq 0 ]; then
-	echo "so many A's to stdin and command line"
 	echo "Usage: $0 [bin dir]"
 	echo "Use absolute paths"
 else
@@ -10,7 +11,7 @@ else
 	
 	for f in *; do
 		echo $f
-		python -c "print 'A'*4097" | timeout 1 ./$f &>/dev/null
-		timeout 1 ./$f $(python -c "print 'A'*4097") &>/dev/null
+		python -c "print 'A'*4097" | timeout .5 ./$f &>/dev/null
+		timeout .5 ./$f $(python -c "print 'A'*4097") &>/dev/null
 	done
 fi
